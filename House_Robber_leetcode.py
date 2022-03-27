38e8ff5976ec40f604d06f89e4fb935b687a74b7
Pranav Tripathi <pranavtripathi79@gmail.com>
2022-03-27 14:21:25 +0530

    Create House_Robber_leetcode.py

---
+++ House_Robber_leetcode.py
@@ -0,0 +1,17 @@
+#memoization solution
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [-1]*(n+1)
+        def solve(index):
+            if index == 0:
+                return nums[index]
+            if index < 0:
+                return 0
+            if dp[index] != -1:
+                return dp[index]
+                
+            dp[index] = max(solve(index-2) + nums[index], solve(index-1))
+            return dp[index]
+             
+        return solve(n-1)
